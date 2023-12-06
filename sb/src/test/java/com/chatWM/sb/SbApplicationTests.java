76cc7f2256d69154d47ee419236491e3555500f0
ThejusT <techdroid11112000@gmail.com>
2023-12-06 12:00:42 +0530

    Basic code added

---
+++ sb/src/test/java/com/chatWM/sb/SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatWM.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
